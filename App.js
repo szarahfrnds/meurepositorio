07b8337fa488911093bae6c83ce188f594b7b31b
szarahfrnds <141147004+szarahfrnds@users.noreply.github.com>
2024-04-04 11:26:07 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+import Routers from './routers'
+
+export default function App() {
+  
+  return (
+   <Routers/> 
+  )
+}
---
+++ routers.jsx
@@ -0,0 +1,167 @@
+import { NavigationContainer } from "@react-navigation/native"
+import { createStackNavigator } from "@react-navigation/stack"
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { Feather } from "@expo/vector-icons"
+import SignIn from './pages/signIn'
+import Home from './pages/home'
+import SignUp from './pages/signUp'
+import Create from "./pages/create"
+import Read from "./pages/read"
+import Update from "./pages/update"
+import Delete from "./pages/delete"
+import Data from "./pages/data"
+
+const Pilha = createStackNavigator()
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+    return (
+        <Tab.Navigator
+            screenOptions={{
+                tabBarStyle: {
+                    backgroundColor: 'black',
+                    paddingBottom: 1,
+                    paddingTop: 1,
+                    borderTopColor: 'transparent'
+                },
+                tabBarActiveTintColor: '#f0f',
+                tabBarInactiveTintColor: '#555'
+            }}
+
+        >
+            <Tab.Screen
+                name="SignIn"
+                component={SignIn}
+                options={{
+                    headerShown: false,
+                    tabBarStyle: { display: 'none' },
+                    tabBarIcon: ({ size, color }) => (
+                        <Feather name="user" size={size} color={color} />
+                    )
+                }}
+            />
+            <Tab.Screen
+                name="Home"
+                component={Home}
+                options={{
+                    // headerShown:false,
+                    tabBarIcon: ({ size, color }) => (
+                        <Feather name="home" size={size} color={color} />
+                    )
+                }}
+            />
+            <Tab.Screen
+                name="Create"
+                component={Create}
+                options={{
+                    // headerShown:false,
+                    tabBarIcon: ({ size, color }) => (
+                        <Feather name="folder-plus" size={size} color={color} />
+                    )
+                }} />
+            <Tab.Screen
+                name="Read"
+                component={Read}
+                options={{
+                    // headerShown:false,
+                    tabBarIcon: ({ size, color }) => (
+                        <Feather name="file" size={size} color={color} />
+                    )
+                }}
+            />
+            <Tab.Screen
+                name="Update"
+                component={Update}
+                options={{
+                    // headerShown:false,
+                    tabBarIcon: ({ size, color }) => (
+                        <Feather name="edit" size={size} color={color} />
+                    )
+                }}
+            />
+            <Tab.Screen
+                name="Delete"
+                component={Delete}
+                options={{
+                    // headerShown:false,
+                    tabBarIcon: ({ size, color }) => (
+                        <Feather name="delete" size={size} color={color} />
+                    )
+                }} />
+            <Tab.Screen
+                name="Data"
+                component={Data}
+                options={{
+                    // headerShown:false,
+                    tabBarIcon: ({ size, color }) => (
+                        <Feather name="hard-drive" size={size} color={color} />
+                    )
+                }} />
+        </Tab.Navigator>
+    );
+}
+
+
+
+export default function Routers() {
+    return (
+        <NavigationContainer>
+            <Pilha.Navigator>
+                <Pilha.Screen
+                    name="Update"
+                    component={Update}
+                    options={{ headerShown: false }}
+                />
+                <Pilha.Screen
+                    name="Delete"
+                    component={Delete}
+                    options={{ headerShown: false }}
+                />
+                <Pilha.Screen
+                    name="Create"
+                    component={Create}
+                    options={{ headerShown: false }}
+                />
+                <Pilha.Screen
+                    name="Read"
+                    component={Read}
+                    options={{ headerShown: false }}
+                />
+                <Pilha.Screen
+                    name="Data"
+                    component={Data}
+                    options={{ headerShown: false }}
+                />
+                <Pilha.Screen
+                    name="MyTabs"
+                    component={MyTabs}
+                    options={{ headerShown: false }}
+                />
+
+                <Pilha.Screen
+                    name="SignIn"
+                    component={SignIn}
+                    options={{ headerShown: false }}
+                />
+
+
+                <Pilha.Screen
+                    name="Home"
+                    component={Home}
+                    options={{ headerShown: false }}
+                />
+
+                <Pilha.Screen
+                    name="SignUp"
+                    component={SignUp}
+                    options={{ headerShown: false }}
+                />
+
+
+
+
+
+            </Pilha.Navigator>
+        </NavigationContainer>
+    )
+}
